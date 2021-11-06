4154a45f3927df0aca5d930504ca3b37d7e54050
Leslye Soares <leslyesoares@gmail.com>
2021-11-06 02:08:02 -0300

    Representar cartas/ Embaralhar cartas

--- script.js
+++ script.js
@@ -1,24 +1,63 @@
+let options = ["bobrossparrot", "explodyparrot", "fiestaparrot", "metalparrot", "revertitparrot", "tripletsparrot", "unicornparrot"];
+let cards = [];
+
 function gamestart(){
+
     let num_card = 0;
     let test = true;
-
     while(test){
         num_card = parseInt(prompt("Com quantas cartas você quer jogar?"));
         if(num_card >= 4 && num_card <= 14 && num_card%2 === 0){
             test = false;
         }
     }
-    let cards = document.querySelector("main");
+
+    create_shuffle_cards(num_card);
+
+    let areacards = document.querySelector("main");
     for(let i = 0; i<num_card; i++){
-        cards.innerHTML += `
+        areacards.innerHTML += `
         <div class="card" data-identifier="card">
         <div class="front-face" data-identifier="front-face">
             <img src="Imagens/front.png" />
         </div>
         <div class="back-face" data-identifier="back-face">
+            <img src="Imagens/${cards[i]}.gif" />
         </div>
         </div>
         `;
     }
+    
 }
+
+function create_shuffle_cards(num_card){
+    let counter = 0;
+    for(let i = 0; i < num_card; i+=2){
+        cards[i] = options[counter];
+        cards[i + 1] = options[counter];
+        counter++;
+    }
+
+    cards.sort(comparator);
+
+}
+
+function comparator() { 
+	return Math.random() - 0.5; 
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 gamestart();
